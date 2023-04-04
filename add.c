ab5727af205d741068455b14a5e1380b54030ff2
ajeet <ajityadaw008>
2023-04-04 23:07:27 +0530

    new project

---
+++ add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+	int p,q,r,s,t,sum;
+	float ave;
+	printf (" enter any five numaber\n");
+//       scanf(" %d %d %d %d %d ",&a,&b,&c,&d,&e);
+       scanf("%d",&p);
+       scanf("%d",&q);
+       scanf("%d",&r);
+       scanf("%d",&s);
+       scanf("%d",&t);
+        sum ==p+q+r+s+t;
+        ave == sum/5.0;
+       printf (" sum = %d\n",sum);
+       printf (" ave = %f\n",ave);
+       return 0;
+
+}
+
+
+
+
+
